1d10891464a8c2a67ebdb109d70f75c1d4bfba5f
Robert Frazier <robert.frazier@ovoenergy.com>
2017-11-27 10:49:17 +0000

    Made it harder for the bot to be exposed as a bot.

--- slackbot/my_plugins/friska.py
+++ slackbot/my_plugins/friska.py
@@ -1,27 +1,39 @@
 import re
+import logging
 from random import choice
 from time import sleep
 
 from slackbot.bot import listen_to
 
+logger = logging.getLogger(__name__)
+
 _FRISKA_REPLIES = [
         "Flat white pls",
+        "Flat white pls.",
         "I'd like a flat white",
-        "Cup of tea please",
-        "Tea please!",
+        "I'd like a flat white.",
+        "Cortado please",
+        "Cortado please.",
+        "Cortado please!",
         "I'll have a flat white please",
         "Flat white please!",
-        "Tea pls!",
-        "Tea pls",
+        "Cortado pls!",
+        "Cortado pls",
         "House bacon please!",
+        "Bacon pls",
         "BACON"
 ]
 
 
-@listen_to('friska.*\?$', re.IGNORECASE)
-# @respond_to('friska ', re.IGNORECASE)
+# Listen for shortish messages that mention Friska and end with a question mark.
+@listen_to('(.){0,20}friska(.){0,15}\?$', re.IGNORECASE)
 def get_some_free_friska(message):
-    channel_name = message._client.channels[message.body['channel']]['name']
-    if channel_name == 'general':
-        sleep(choice([1, 2, 3]))
-        message.send(choice(_FRISKA_REPLIES))
+    try:
+        channel_name = message._client.channels[message.body['channel']]['name']
+        if channel_name == 'bristol_chitchat':
+            sleep(choice([1, 2, 3]))
+            message.send(choice(_FRISKA_REPLIES))
+    except Exception as e:
+        # We never want to invoke the normal error response mechanisms, lest they
+        # find out that this is actually a bot.
+        logger.exception("Ooopsy, something went wrong")
